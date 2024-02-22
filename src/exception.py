1d99b7e8e9c952e80f1fe662e793d0a14a522966
Vaishnavi Mhaske <vmhaske059@gmail.com>
2024-02-21 20:58:35 -0500

    Logging and Exception Handling

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+'''For Exception Handling'''
+import sys
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info() #exc_tb will give where the exception has occured
+    file_name = exc_tb.tb_frame.f_code.co_filename #Custom exception handling in pyhton document
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno,str(error))
+
+    return error_message
+    
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys): #Constructor
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+
+        def __str__(self):
+            return self.error_message 
+        
+            
+
+
+    
+
